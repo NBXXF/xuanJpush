e5928f16ca182125ef1af4d3bbfe451f05491fcb
xuanyouwu@163.com <axuan1314>
2017-09-04 11:51:20 +0800

    Initial commit

---
+++ JPushExample(707612)/build/generated/source/buildConfig/androidTest/debug/com/xuan/jpush/test/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.xuan.jpush.test;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.xuan.jpush.test";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = -1;
+  public static final String VERSION_NAME = "";
+}
